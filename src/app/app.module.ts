95c15b6fcd2365fbec6ddbece211ee82082946af
BertelsBenjamin <bertels123@gmail.com>
2019-12-16 15:34:06 +0100

    routing - home, pricing, contact, login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,31 @@ import { Routes, RouterModule } from "@angular/router";
 // COMPONENTS
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { HomeHeaderComponent } from "./home-header/home-header.component";
+import { HomeFooterComponent } from "./home-footer/home-footer.component";
+import { PricingComponent } from "./pricing/pricing.component";
+import { ContactComponent } from "./contact/contact.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "pricing", component: PricingComponent },
+  { path: "contact", component: ContactComponent },
   { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HomeHeaderComponent,
+    HomeFooterComponent,
+    HomeComponent,
+    PricingComponent,
+    ContactComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
