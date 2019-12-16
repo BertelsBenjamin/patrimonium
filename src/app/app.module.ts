7ae492395dea7f59d2da95497cfb9253720029e5
BertelsBenjamin <bertels123@gmail.com>
2019-12-16 11:18:53 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
+// MODULES
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { RoutingModule } from "./routing/routing.module";
+import { Routes, RouterModule } from "@angular/router";
+
+// COMPONENTS
 import { AppComponent } from "./app.component";
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from "./login/login.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", component: LoginComponent }
+];
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RoutingModule,
+    RouterModule.forRoot(routes)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
